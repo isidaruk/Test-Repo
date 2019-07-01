98ca1667052da8b677548706585d6ac8e6bd895a
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-07-01 11:09:57 +0300

    Removed Some Code

--- script.py
+++ script.py
@@ -1,24 +1,5 @@
-import math
-import sys
-from os import rename
-
 import requests
 
-# print sys.version
-# print(sys.version)
-print(sys.executable)
 
-
-def greet(who_to_greet):
-    # test = "Test"
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
-# print(greet('World'))
-# print(greet('Corey'))
 r = requests.get("https://coreyms.com")
 print(r.status_code)
-
-# name = input("Your Name? ")
-# print("Hello,", name)
